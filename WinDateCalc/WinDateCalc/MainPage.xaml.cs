05f15e4c062373e389f3c2b65d0ff7156f88ab8a
Giulio Sorrentino <numeroide84@hotmail.com>
2022-08-10 22:53:12 +0200

    Aggiungere i file di progetto.

---
+++ WinDateCalc/WinDateCalc/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WinDateCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        private static MyUserSettings mus = new MyUserSettings();
+        public MainPage()
+        {
+            InitializeComponent();
+            data.Date = mus.data.Date;
+        }
+
+        private void calcola_Click(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            TimeSpan differenza = data.Date.Date-d;
+            risultato.Text ="Mancano " + differenza.Days + " giorni.";
+            mus.data = data.Date;
+            mus.Save();
+        }
+    }
+}
